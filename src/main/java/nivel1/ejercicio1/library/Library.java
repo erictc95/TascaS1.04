c5aa2992871ff5bd967358124605b872b650c87d
erictc95 <maxerictc95@hotmail.com>
2026-02-12 15:20:46 +0100

    Version 1 - Create the Library Class.

---
+++ src/main/java/nivel1/ejercicio1/library/Library.java
@@ -0,0 +1,13 @@
+package nivel1.ejercicio1.library;
+
+import java.util.ArrayList;
+
+public class Library {
+    ArrayList<String> books = new ArrayList<>();
+
+    public Library() {
+        books = new ArrayList<>();
+    }
+
+
+}
