6d6af46fb3ef7693d29b1fe4097515985408818a
erictc95 <maxerictc95@hotmail.com>
2026-02-12 18:21:31 +0100

    Version 9 - Created all tests.

--- src/main/java/nivel1/ejercicio1/library/LibraryTest.java
+++ src/main/java/nivel1/ejercicio1/library/LibraryTest.java
@@ -1,6 +1,9 @@
 package nivel1.ejercicio1.library;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LibraryTest {
@@ -55,5 +58,66 @@ public class LibraryTest {
 
         assertNull(library.getBookForPosition(1));
         assertNull(library.getBookForPosition(5));
+    }
+
+    @Test
+    void addBookInEspecificPosition() {
+        Library library = new Library();
+
+        library.addBook("Bob el manetes");
+        library.addBook("El rey Leon");
+
+        //otra manera distinta de comprobar que funciona
+        boolean added = library.addBookInPosition(1, "Pocahontas");
+
+        assertTrue(added);
+    }
+
+    @Test
+    void erasedBook() {
+        Library library = new Library();
+        library.addBook("El rey Leon");
+        library.addBook("Aladin");
+        library.addBook("Pocahontas");
+        library.addBook("El libro de la selva");
+
+        library.eraseBook("Aladin");
+
+        assertEquals(3, library.getAllBooks().size());
+        assertFalse(library.getAllBooks().contains("Aladin"));
+        assertTrue(library.eraseBook("Aladin"));
+    }
+
+    @Test
+    void sortedListAndOriginalListNotModified() {
+        Library library = new Library();
+        library.addBook("El rey Leon");
+        library.addBook("Aladin");
+        library.addBook("Pocahontas");
+        library.addBook("El libro de la selva");
+
+        ArrayList<String> original = new ArrayList<>(library.getAllBooks());
+        ArrayList<String> sorted = library.getSortedBooks();
+
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("Aladin");
+        expected.add("El libro de la selva");
+        expected.add("El rey Leon");
+        expected.add("Pocahontas");
+
+        assertEquals(expected, sorted);
+        assertEquals(original, library.getAllBooks());
+    }
+
+    @Test
+    void duplicatesNotAllowed() {
+        Library library = new Library();
+
+        boolean firstAdd = library.addBook("Aladin");
+        boolean secondAdd = library.addBook("Aladin");
+
+        assertTrue(firstAdd);
+        assertFalse(secondAdd);
+        assertEquals(1, library.getAllBooks().size());
     }
 }
