8bab333db1965f2dc60f0e90fbf47dc895aa2924
erictc95 <maxerictc95@hotmail.com>
2026-02-12 17:25:34 +0100

    Version 8 - Created 4 first tests.

--- src/main/java/nivel1/ejercicio1/library/LibraryTest.java
+++ src/main/java/nivel1/ejercicio1/library/LibraryTest.java
@@ -6,10 +6,11 @@ import static org.junit.jupiter.api.Assertions.*;
 public class LibraryTest {
     @Test
     void testAddBook () {
+        //Arrange
         Library library = new Library();
-
+        //Act
         library.addBook("Pocahontas");
-
+        //Assert
         assertEquals(1, library.getAllBooks().size());
     }
 
@@ -18,5 +19,41 @@ public class LibraryTest {
         Library library = new Library();
 
         assertNotNull(library.getAllBooks());
+    }
+
+    @Test
+    void correctSizeLaterAddBooks() {
+        Library library = new Library();
+
+        library.addBook("Bob el manetes");
+        library.addBook("El rey Leon");
+        library.addBook("Aladin");
+
+        assertEquals(3, library.getAllBooks().size());
+    }
+
+    @Test
+    void booksInCorrectPosition() {
+        Library library = new Library();
+
+        library.addBook("Bob el manetes");
+        library.addBook("El rey Leon");
+        library.addBook("Aladin");
+
+        assertEquals("Bob el manetes", library.getBookForPosition(0));
+        assertEquals("El rey Leon", library.getBookForPosition(1));
+        assertEquals("Aladin", library.getBookForPosition(2));
+    }
+
+    @Test
+    void obtainBookForPosition() {
+        Library library = new Library();
+
+        library.addBook("Bob el manetes");
+        library.addBook("El rey Leon");
+        library.addBook("Aladin");
+
+        assertNull(library.getBookForPosition(1));
+        assertNull(library.getBookForPosition(5));
     }
 }
