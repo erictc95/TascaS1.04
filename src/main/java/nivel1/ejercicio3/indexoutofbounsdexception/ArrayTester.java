8ef6a3466c49f7c79e12813744684fb8808c8f62
erictc95 <maxerictc95@hotmail.com>
2026-02-16 12:55:01 +0100

    Version 1 - Created method accessInvalidIndex with the Array of letters.

---
+++ src/main/java/nivel1/ejercicio3/indexoutofbounsdexception/ArrayTester.java
@@ -0,0 +1,14 @@
+package nivel1.ejercicio3.indexoutofbounsdexception;
+
+import java.util.ArrayList;
+
+public class ArrayTester {
+
+    public void accessInvalidIndex() {
+        String[] letters = {"A", "B", "C"};
+
+        String letter = letters[5];
+    }
+
+
+}
