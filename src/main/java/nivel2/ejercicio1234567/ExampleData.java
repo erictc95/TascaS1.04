1c5a4b455c3f6d8a8a032f97bd639a95b8d8876d
erictc95 <maxerictc95@hotmail.com>
2026-02-17 13:16:53 +0100

    Version 1 - create Exampledata class and do the pertinent methods of exercises.

---
+++ src/main/java/nivel2/ejercicio1234567/ExampleData.java
@@ -0,0 +1,35 @@
+package nivel2.ejercicio1234567;
+
+import java.util.*;
+
+public class ExampleData {
+
+
+    public int[] getArray1() {
+        return new int[]{1, 2, 3};
+    }
+
+    public int[] getArray2() {
+        return new int[]{1, 2, 3};
+    }
+
+    public List<Object> getList() {
+        List<Object> list = new ArrayList<>();
+        list.add("Hola");
+        list.add(42);
+        list.add(3.14);
+        return list;
+    }
+
+    public Map<String, String> getMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("key1", "value1");
+        map.put("key2", "value2");
+        return map;
+    }
+
+    public Optional<String> getEmptyOptional() {
+        return Optional.empty();
+    }
+
+}
